5112d0c36434d74b9fb91cd1c7b808c4160ad1c4
Adrien <chuadrien2003@gmail.com>
2025-05-03 00:49:48 +0200

    FIX: syntax

--- src/routine.c
+++ src/routine.c
@@ -6,43 +6,24 @@
 /*   By: achu <achu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 02:03:27 by achu              #+#    #+#             */
-/*   Updated: 2025/05/02 00:28:30 by achu             ###   ########.fr       */
+/*   Updated: 2025/05/03 00:49:31 by achu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-static bool	check_death(t_philo *philo)
-{
-	bool	is_dead;
-
-	pthread_mutex_lock(&philo->data->m_over);
-	is_dead = philo->data->is_dead;
-	if (!is_dead)
-	{
-		if (get_time_ms() - philo->last_meal > philo->data->time_die)
-		{
-			log_status(*philo, "died");
-			philo->data->is_dead = true;
-			is_dead = true;
-		}
-	}
-	pthread_mutex_unlock(&philo->data->m_over);
-	return (is_dead);
-}
-
 static bool	check_thinking(t_philo *philo)
 {
 	if (check_death(philo))
 		return (false);
-	log_status(*philo, "is thinking");
+	log_status(philo, "is thinking");
 	pthread_mutex_lock(philo->left);
 	if (check_death(philo))
 	{
 		pthread_mutex_unlock(philo->left);
 		return (false);
 	}
-	log_status(*philo, "has taken a fork");
+	log_status(philo, "has taken a fork");
 	pthread_mutex_lock(philo->right);
 	if (check_death(philo))
 	{
@@ -50,7 +31,7 @@ static bool	check_thinking(t_philo *philo)
 		pthread_mutex_unlock(philo->right);
 		return (false);
 	}
-	log_status(*philo, "has taken a fork");
+	log_status(philo, "has taken a fork");
 	return (true);
 }
 
@@ -58,7 +39,7 @@ static bool	check_eating(t_philo *philo)
 {
 	long	start;
 
-	log_status(*philo, "is eating");
+	log_status(philo, "is eating");
 	philo->last_meal = get_time_ms();
 	start = get_time_ms();
 	while (get_time_ms() - start <= philo->data->time_eat)
@@ -86,7 +67,7 @@ static bool	check_sleeping(t_philo *philo)
 
 	if (check_death(philo))
 		return (false);
-	log_status(*philo, "is sleeping");
+	log_status(philo, "is sleeping");
 	start = get_time_ms();
 	while (get_time_ms() - start <= philo->data->time_sleep)
 	{
@@ -108,7 +89,7 @@ void	*routine(void *arg)
 	if (philo->data->num_philo == 1)
 	{
 		pthread_mutex_lock(philo->left);
-		log_status(*philo, "has taken a fork");
+		log_status(philo, "has taken a fork");
 		while (!check_death(philo))
 			usleep(100); // Wait until dead
 		pthread_mutex_unlock(philo->left);
